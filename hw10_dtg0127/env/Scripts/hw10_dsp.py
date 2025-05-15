8d609d4141710ccbef32e9ffad23f7291d6572ee
danishgalebotswe <danishgalebotswe2026@u.northwestern.edu>
2025-05-15 10:26:32 -0500

    started hw10

---
+++ hw10_dtg0127/env/Scripts/hw10_dsp.py
@@ -0,0 +1,24 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = [] # column 0
+data1 = [] # column 1
+
+with open('sigA.csv') as f:
+    # open the csv file
+    reader = csv.reader(f)
+    for row in reader:
+        # read the rows one by one
+        t.append(float(row[0])) # leftmost column
+        data1.append(float(row[1])) # second column
+
+# for i in range(len(t)):
+#     # print the data to verify it was read
+#     print(str(t[i]) +", " + str(data1[i]))
+
+plt.plot(t,data1)
+plt.xlabel('Time (sec)')
+plt.ylabel('Signal')
+plt.title('Signal vs Time')
+plt.show()
