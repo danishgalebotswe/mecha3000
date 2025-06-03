14b6ed7b8c1431bc5ee3f206ea1d361e04045650
danishgalebotswe <danishgalebotswe2026@u.northwestern.edu>
2025-06-03 11:04:12 -0500

    Update hw16_dtg0127.c

--- hw16_dtg0127/hw16_dtg0127.c
+++ hw16_dtg0127/hw16_dtg0127.c
@@ -38,17 +38,18 @@ int main()
 
 
 void pwm_pin_setup(void) {
-    gpio_init(PWM_B_EN);
+    // gpio_init(PWM_B_EN);
     gpio_init(PWM_B_PH);
 
     // gpio_set_dir(PWM_A_EN, true);
-    // gpio_set_dir(PWM_A_PH, true);
+    gpio_set_dir(PWM_B_PH, true);
+    gpio_put(PWM_B_PH, 0);
 
     gpio_set_function(PWM_B_EN, GPIO_FUNC_PWM);
-    gpio_set_function(PWM_B_PH, GPIO_FUNC_PWM);
+    // gpio_set_function(PWM_B_PH, GPIO_FUNC_PWM);
 
     // Find out which PWM slice is connected to PH (it's slice 0)
-    slice_num = pwm_gpio_to_slice_num(PWM_B_PH);
+    slice_num = pwm_gpio_to_slice_num(PWM_B_EN);
 
     
     pwm_set_wrap(slice_num, wrap);
@@ -58,17 +59,29 @@ void pwm_pin_setup(void) {
 }
 
 void setMotor(int dutyCycle) {
+
+    // Clamp value and calculate PWM level
+    if (dutyCycle > 100) dutyCycle = 100;
+    if (dutyCycle < -100) dutyCycle = -100;
+
     uint16_t level = (uint16_t)((abs(dutyCycle) / 100.0) * wrap);
 
     if (dutyCycle > 0) {
-        pwm_set_chan_level(slice_num, PWM_CHAN_A, 0);        // PH = 0
-        pwm_set_chan_level(slice_num, PWM_CHAN_B, level);    // EN = PWM
+        // pwm_set_chan_level(slice_num, PWM_CHAN_A,);        // PH = 0
+        // pwm_set_chan_level(slice_num, PWM_CHAN_B, level);    // EN = PWM
+
+        gpio_put(PWM_B_PH, 0);  // Forward
+        pwm_set_chan_level(slice_num, pwm_gpio_to_channel(PWM_B_EN), level);
+
     } else if (dutyCycle < 0) {
-        pwm_set_chan_level(slice_num, PWM_CHAN_A, level);    // PH = PWM
-        pwm_set_chan_level(slice_num, PWM_CHAN_B, 0);        // EN = 0
+        gpio_put(PWM_B_PH, 1);  // Reverse
+        pwm_set_chan_level(slice_num, pwm_gpio_to_channel(PWM_B_EN), 100-level);
+
+        // pwm_set_chan_level(slice_num, PWM_CHAN_A, level);    // PH = PWM
+        // pwm_set_chan_level(slice_num, PWM_CHAN_B, 0);        // EN = 0
     } else {
-        pwm_set_chan_level(slice_num, PWM_CHAN_A, 0);        // Both = 0
-        pwm_set_chan_level(slice_num, PWM_CHAN_B, 0);
+        pwm_set_chan_level(slice_num, pwm_gpio_to_channel(PWM_B_EN), 0);        // Both = 0
+        // pwm_set_chan_level(slice_num, PWM_CHAN_B, 0);
     }
 }
 
